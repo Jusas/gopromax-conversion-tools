116c62ee926a19877eab16ed65e27d0d43283bb4
Jussi Saarivirta <jusasi@gmail.com>
2025-08-04 16:14:25 +0300

    Publishing workflow

--- publish/scripts/build-singlefile.sh
+++ publish/scripts/build-singlefile.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 # Build a single-file executable.
 # e.g.
-# ./<script> linux-x64 /tmp/publish
+# ./<script> linux-x64 0.9.0.0 /tmp/publish
 
 ARCH="$1"
-PUBLISH_ROOT="$2"
+VERSION="$2"
+PUBLISH_ROOT="$3"
+
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 CSPROJ="${SCRIPT_DIR}/../../src/VideoConversionApp/VideoConversionApp.csproj"
 
 dotnet clean "${CSPROJ}"
-dotnet publish -r "${ARCH}" -c Release --self-contained true -p:DebugType=embedded -p:IncludeAllContentForSelfExtract=true -p:EnableCompressionInSingleFile=true -o "${PUBLISH_ROOT}/${ARCH}" "${CSPROJ}"
+dotnet publish -r "${ARCH}" -c Release  --self-contained true -p:Version=$VERSION -p:DebugType=embedded -p:IncludeAllContentForSelfExtract=true -p:EnableCompressionInSingleFile=true -o "${PUBLISH_ROOT}/${ARCH}" "${CSPROJ}"
 
-cat "${CSPROJ}" | grep AssemblyVersion | sed 's%\s*<AssemblyVersion>\(.*\)</AssemblyVersion>%\1%' > "${PUBLISH_ROOT}/${ARCH}/version.txt"
+# cat "${CSPROJ}" | grep AssemblyVersion | sed 's%\s*<AssemblyVersion>\(.*\)</AssemblyVersion>%\1%' > "${PUBLISH_ROOT}/${ARCH}/version.txt"
+# echo "${VERSION}" > "${PUBLISH_ROOT}/${ARCH}/version.txt"
--- publish/scripts/create-appimage.sh
+++ publish/scripts/create-appimage.sh
@@ -10,43 +10,53 @@ set -e
 # 4. Package the folder to an AppImage using AppImageKit
 
 # Usage:
-# ./<script> linux-x64 x86_64
+# ./<script> linux-x64 x86_64 0.9.0.0 /tmp/my/release
+
+# Dotnet architecture string
 ARCH="$1"
+# Appimagekit architecture string
 APPIMAGE_ARCH="$2"
+# Version number, will override the CSPROJ number
+VERSION="$3"
+# Output directory - if left out, will default to under /tmp
+OUTPUT_DIR="$4"
+
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 NOW=$( date +"%y%m%d%H%M%S" )
 TEMPDIR="/tmp/maxvideoconverter-bundle-${ARCH}-${NOW}"
+OUTPUT_DIR=${OUTPUT_DIR:-"$TEMPDIR"}
 APP_ASSEMBLY_NAME="VideoConversionApp"
 FINAL_APP_NAME="MAXVideoConverter"
 
-
+# Grab the releases from github for these.
 EXIFTOOL_VER="13.33"
 EXIFTOOL_URL="https://github.com/exiftool/exiftool/archive/refs/tags/${EXIFTOOL_VER}.tar.gz"
 APPIMAGEKIT_URL="https://github.com/AppImage/appimagetool/releases/download/continuous/appimagetool-x86_64.AppImage"
 
 
-echo "** Creating temporary directory and copying bundle structure there **"
-mkdir -p "${TEMPDIR}"
-cp -R "${SCRIPT_DIR}/../appimage/MAXVideoConverter.AppDir" "${TEMPDIR}"
-APPDIR="${TEMPDIR}/MAXVideoConverter.AppDir"
+echo "** Creating output directory and copying bundle structure there **"
+mkdir -p "${OUTPUT_DIR}"
+cp -R "${SCRIPT_DIR}/../appimage/MAXVideoConverter.AppDir" "${OUTPUT_DIR}"
+APPDIR="${OUTPUT_DIR}/MAXVideoConverter.AppDir"
 
 echo "** Downloading appimagekit and exiftool **"
-curl -L -o "${TEMPDIR}/appimagetool" "${APPIMAGEKIT_URL}" && chmod +x "${TEMPDIR}/appimagetool"
-curl -L -o "${TEMPDIR}/exiftool.tgz" "${EXIFTOOL_URL}" && tar --directory="${TEMPDIR}" -xf "${TEMPDIR}/exiftool.tgz"
+curl -L -o "${OUTPUT_DIR}/appimagetool" "${APPIMAGEKIT_URL}" && chmod +x "${OUTPUT_DIR}/appimagetool"
+curl -L -o "${OUTPUT_DIR}/exiftool.tgz" "${EXIFTOOL_URL}" && tar --directory="${OUTPUT_DIR}" -xf "${OUTPUT_DIR}/exiftool.tgz"
 
 echo "** Moving exiftool files under AppDir **"
-mv "${TEMPDIR}/exiftool-${EXIFTOOL_VER}" "${APPDIR}/usr/bin/external-utils"
+mv "${OUTPUT_DIR}/exiftool-${EXIFTOOL_VER}" "${APPDIR}/usr/bin/external-utils"
 ln -r -s "${APPDIR}/usr/bin/external-utils/exiftool-${EXIFTOOL_VER}/exiftool" "${APPDIR}/usr/bin/external-utils/exiftool"
 
 echo "** Building application **"
-. "${SCRIPT_DIR}/build-singlefile.sh" "${ARCH}" "${TEMPDIR}"
-cp "${TEMPDIR}/${ARCH}/${APP_ASSEMBLY_NAME}" "${APPDIR}/usr/bin/${FINAL_APP_NAME}"
+. "${SCRIPT_DIR}/build-singlefile.sh" "${ARCH}" "${VERSION}" "${OUTPUT_DIR}"
+cp "${OUTPUT_DIR}/${ARCH}/${APP_ASSEMBLY_NAME}" "${APPDIR}/usr/bin/${FINAL_APP_NAME}"
 
-APPVER=$(cat "${TEMPDIR}/${ARCH}/version.txt")
+# APPVER=$(cat "${OUTPUT_DIR}/${ARCH}/version.txt")
+APPVER=$VERSION
 
 echo "** Creating AppImage **"
-mkdir -p "${TEMPDIR}/release"
-ARCH="$APPIMAGE_ARCH" "${TEMPDIR}/appimagetool" "${APPDIR}" "${TEMPDIR}/release/${FINAL_APP_NAME}-${APPVER}-${APPIMAGE_ARCH}.AppImage"
+mkdir -p "${OUTPUT_DIR}/release"
+ARCH="$APPIMAGE_ARCH" "${OUTPUT_DIR}/appimagetool" "${APPDIR}" "${OUTPUT_DIR}/release/${FINAL_APP_NAME}-${APPVER}-${APPIMAGE_ARCH}.AppImage"
 
 echo "** DONE! **"
