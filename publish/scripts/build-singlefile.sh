3650feae80a93d4b46b0a7c8269bbada31a300bd
Jussi Saarivirta <jusasi@gmail.com>
2025-08-04 15:04:55 +0300

    Clean before build

--- publish/scripts/build-singlefile.sh
+++ publish/scripts/build-singlefile.sh
@@ -8,6 +8,7 @@ PUBLISH_ROOT="$2"
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 CSPROJ="${SCRIPT_DIR}/../../src/VideoConversionApp/VideoConversionApp.csproj"
 
+dotnet clean "${CSPROJ}"
 dotnet publish -r "${ARCH}" -c Release --self-contained true -p:DebugType=embedded -p:IncludeAllContentForSelfExtract=true -p:EnableCompressionInSingleFile=true -o "${PUBLISH_ROOT}/${ARCH}" "${CSPROJ}"
 
 cat "${CSPROJ}" | grep AssemblyVersion | sed 's%\s*<AssemblyVersion>\(.*\)</AssemblyVersion>%\1%' > "${PUBLISH_ROOT}/${ARCH}/version.txt"
